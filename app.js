fa793d0313be87aaa1b7bdbd32f02c576bb1a2e5
Dong Gyu <nninnnin7@gmail.com>
2020-03-08 00:00:20 +0900

    0308

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const partials = require('express-partials');
 
+const indexRouter = require('./routes/router');
+
 // create server
 const app = express();
 
@@ -15,72 +17,7 @@ app.use(partials());
 
 app.engine('html', require('ejs').renderFile);
 
-
-
-
-// routings
-app.get('/',(req,res)=>{
-    res.render('index',{
-        imgs:[
-            {
-                id:1,
-            },
-            {
-                id:2,
-            },
-            {
-                id:3,
-            }
-        ]
-    });
-});
-
-app.get('/profile',(req,res)=>{
-    res.render('profile.html');
-});
-
-app.get('/project',(req,res)=>{
-    res.render('project.html');
-});
-
-app.get('/gallery',(req,res)=>{
-    res.render('gallery.html');
-});
-
-app.get('/diary',(req,res)=>{
-    res.render('diary',{posts:[
-        {
-            id:1,
-            title:'Akira',
-            content:'아키라는 2020년 네오도쿄를 배경으로 한다'
-        },
-        {
-            id:2,
-            title:'Flavors of Youth',
-            content:'최근 나는 일본 문화를 다시 사랑하게 되었다. 그런 연유로 다시금 일본의 애니메이션을 보고싶다는 마음을 갖게 되었는데, 잔잔한 사랑의 아픔과 여운을 느낄 수 있는 좋은 영화였다고 생각한다.'
-        },
-        {
-            id:3,
-            title:'Miss little sunshine',
-            content:'미스 리틀 선샤인, 잔잔하고 따듯한 영화.'
-        }
-    ]});
-});
-
-app.get('/diary/post',function(req,res){
-    res.render('post', {
-        profile:{
-            name:'justindglee',
-            age:35
-        }
-    });
-});
-
-app.get('/visitor',(req,res)=>{
-    res.render('visitor.html')
-});
-
-
+app.use('/', indexRouter);
 
 app.listen(8000, function(){
     console.log("express server heard on 8000");
---
+++ routes/router.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+
+const posts = require('../public/data/diary.json');
+
+// home
+router.get('/',(req,res)=>{
+    res.render('index',{
+        imgs:[
+            {
+                id:1,
+            },
+            {
+                id:2,
+            },
+            {
+                id:3,
+            }
+        ]
+    });
+});
+
+// profile
+router.get('/profile',(req,res)=>{
+    res.render('profile.html');
+});
+// project
+router.get('/project',(req,res)=>{
+    res.render('project.html');
+});
+// gallery
+router.get('/gallery',(req,res)=>{
+    res.render('gallery.html');
+});
+// diary
+router.get('/diary',(req,res)=>{
+    res.render('diary',{posts:posts});
+});
+// diary post
+router.get('/diary/post',function(req,res){
+    res.render('post', {
+        profile:{
+            name:'justindglee',
+            age:35
+        }
+    });
+});
+//visitor
+router.get('/visitor',(req,res)=>{
+    res.render('visitor.html')
+});
+
+
+module.exports = router;
