5143093657eb2c3d2a28c8c8ced9fe98fb0b53aa
donggyu <nninnnin7@gmail.com>
2020-04-05 01:26:58 +0900

    mongo to mlab

--- app.js
+++ app.js
@@ -64,7 +64,11 @@ app.use('/visitor', visitorRouter);
 mongoose.Promise = global.Promise;
 
 // Connect to MongoDB server
-mongoose.connect('mongodb://localhost:27017/test',{
+// mongoose.connect('mongodb://localhost:27017/test',{
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// })
+mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
