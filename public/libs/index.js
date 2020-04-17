b172afdafa1f41a9eb1e9b1cb6fb058349c4e051
donggyu <nninnnin7@gmail.com>
2020-04-17 22:51:46 +0900

    -

--- public/libs/index.js
+++ public/libs/index.js
@@ -1,12 +1,10 @@
 // Present index page after loader
 window.onload = function(){
-    setTimeout(()=>{
-        const main = document.querySelector('main');
-        main.classList.add('fade-in');
-        const loader = document.getElementById('loader');
-        // loader.style = "display:none;";
-        document.body.style="display:block;"
-    },8000)
+    const main = document.querySelector('main');
+    main.classList.add('fade-in');
+    // const loader = document.getElementById('loader');
+    // loader.style = "display:none;";
+    document.body.style="display:block;"
 }
 
 // window.addEventListener('DOMContentLoaded',()=>{
