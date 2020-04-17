3773d4573a1464f941157107cc4d2c0396c00816
donggyu <nninnnin7@gmail.com>
2020-04-17 22:32:40 +0900

    prerender 고려중

--- public/libs/index.js
+++ public/libs/index.js
@@ -1,8 +1,14 @@
 // Present index page after loader
 window.onload = function(){
-    const main = document.querySelector('main');
-    main.classList.add('fade-in');
-    const loader = document.getElementById('loader');
-    loader.style = "display:none;";
-    document.body.style="display:block;"
+    setTimeout(()=>{
+        const main = document.querySelector('main');
+        main.classList.add('fade-in');
+        const loader = document.getElementById('loader');
+        // loader.style = "display:none;";
+        document.body.style="display:block;"
+    },8000)
 }
+
+// window.addEventListener('DOMContentLoaded',()=>{
+    
+// })
