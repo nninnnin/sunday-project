d78f3a2355281b6ce3013aa47c96ca9aa49eeb0c
donggyu <nninnnin7@gmail.com>
2020-04-18 12:37:26 +0900

    loader added on index and diary view

---
+++ public/libs/diary.js
@@ -0,0 +1,19 @@
+window.onload=()=>{
+    //이미지 등 로딩 이후 (onload) loader 없애기
+    const loader = document.getElementById('loader');
+    loader.style = "display:none;";
+    
+    // 모든 이미지 등 로딩 이후 main과 footer 보이기
+    const main = document.querySelector('main');
+    main.style = "display:block;";
+    const footer = document.querySelector('footer');
+    footer.style = "display:block;";
+}
+
+
+const writeBtn = document.getElementById('writeButton');
+        writeBtn.addEventListener('click',()=>{
+            window.location.href = '/diary/write';
+        })
+
+
--- public/libs/index.js
+++ public/libs/index.js
@@ -1,12 +1,12 @@
 // 메인페이지는 다 그려지고 보이는게 예쁠 것 같아서
-window.onload = function(){
-    const main = document.querySelector('main');
-    main.classList.add('fade-in');
-    // const loader = document.getElementById('loader');
-    // loader.style = "display:none;";
-    document.body.style="display:block;"
-}
+window.onload = ()=>{
+    const loader = document.getElementById('loader');
+    loader.style = "display:none;";
 
-// window.addEventListener('DOMContentLoaded',()=>{
-    
-// })
+    const main = document.querySelector('main');
+    main.style="display:grid;"
+    const footer = document.querySelector('footer');
+    footer.style="display:block;"
+
+    main.classList.add('fade-in');
+}
