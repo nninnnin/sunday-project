91383aad2092d6e5dce8f212ba047a0beffb0d17
nninnnin <nninnnin7@gmail.com>
2020-05-22 15:24:14 +0900

    some fixed
    footer position relative when scroll
    
    visitor font size

--- server/public/libs/common.js
+++ server/public/libs/common.js
@@ -1,3 +1,8 @@
+// 페이지별 footer 위치선정
+if (document.body.offsetHeight > window.innerHeight) {
+  document.querySelector("footer").style.position = "relative";
+}
+
 // navigation link 누르면 fade out
 const nav_items = Array.from(document.querySelector("nav").children);
 nav_items.forEach((nav_item) => {
@@ -88,3 +93,35 @@ if (location.pathname !== "/") {
 //   const imgId = "B8tCX49plkG";
 //   getPicture(imgId);
 // }
+
+// // The debounce function receives our function as a parameter
+const debounce = (fn) => {
+  // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+  let frame;
+
+  // The debounce function returns a new function that can receive a variable number of arguments
+  return (...params) => {
+    // If the frame variable has been defined, clear it now, and queue for next frame
+    if (frame) {
+      cancelAnimationFrame(frame);
+    }
+
+    // Queue our function call for the next frame
+    frame = requestAnimationFrame(() => {
+      // Call our function and pass any params we received
+      fn(...params);
+    });
+  };
+};
+
+// Reads out the scroll position and stores it in the data attribute
+// so we can use it in our stylesheets
+const storeScroll = () => {
+  document.documentElement.dataset.scroll = window.scrollY;
+};
+
+// Listen for new scroll events, here we debounce our `storeScroll` function
+document.addEventListener("scroll", debounce(storeScroll), { passive: true });
+
+// Update scroll position for first time
+storeScroll();
